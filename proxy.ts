10f378a1827de6c4b647fdd18300f860fb8f05f5
Catherine Paulen <myra.renov@gmail.com>
2026-05-06 15:21:18 +0200

    disable middleware test

--- proxy.ts
+++ proxy.ts
@@ -1,13 +1,5 @@
-import createMiddleware from 'next-intl/middleware';
-import { defineRouting } from 'next-intl/routing';
-
-const routing = defineRouting({
-  locales: ['fr', 'en', 'de'],
-  defaultLocale: 'fr',
-});
-
-export default createMiddleware(routing);
+export default function middleware() {}
 
 export const config = {
-  matcher: ['/((?!_next|_vercel|api|.*\\..*).*)'],
+  matcher: [],
 };
