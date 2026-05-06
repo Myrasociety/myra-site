36acabb0bd48176c032247cf259ec1869ff21f8b
Catherine Paulen <myra.renov@gmail.com>
2026-05-06 15:08:54 +0200

    fix proxy matcher

--- proxy.ts
+++ proxy.ts
@@ -3,9 +3,9 @@ import createMiddleware from 'next-intl/middleware';
 export default createMiddleware({
   locales: ['fr', 'en', 'de'],
   defaultLocale: 'fr',
-  localeDetection: true,
+  localeDetection: false,
 });
 
 export const config = {
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)', '/'],
+  matcher: ['/((?!_next|_vercel|api|.*\\..*).*)'],
 };
